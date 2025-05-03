c9652178d8e98b4b775602955a60478a52ca35c0
Leandro Abud Miguel Jacob <leandro.jacob@meta.com.br>
2025-05-03 11:43:10 -0300

    atualização testes central

--- tests/e2e/central.spec.js
+++ tests/e2e/central.spec.js
@@ -72,28 +72,61 @@ test('deve buscar um produto pelo nome', async ({ page }) => {
 
 test('deve adicionar um produto ao carrinho', async ({ page }) => {
 
-    await page.goto('https://qas.central-brf.com.br/login');
+    await page.goto('https://qas.central-brf.com.br/login')
 
-    await page.getByPlaceholder("e-mail").fill('terezinha.075@uorak.com');
-    await page.getByPlaceholder("Senha").fill('Central@24');
-    await page.locator('button:has-text("Entrar")').click();
+    await page.getByPlaceholder("e-mail").fill('terezinha.075@uorak.com')
+    await page.getByPlaceholder("Senha").fill('Central@24')
+    await page.locator('button:has-text("Entrar")').click()
 
-    const loginUser = page.locator('.menu-trigger-p.slds-text-align_right.slds-truncate.slds-text-title_bold.lwc-5uc1gleli5');
-    await expect(loginUser).toHaveText("TEREZINHA DE JESUS MENDES GAMA");
+    const loginUser = page.locator('.menu-trigger-p.slds-text-align_right.slds-truncate.slds-text-title_bold.lwc-5uc1gleli5')
+    await expect(loginUser).toHaveText("TEREZINHA DE JESUS MENDES GAMA")
     
-    await page.goto('https://qas.central-brf.com.br/global-search/Coxas%20e%20Sobrecoxas%20de%20Frango');
+    await page.goto('https://qas.central-brf.com.br/global-search/Coxas%20e%20Sobrecoxas%20de%20Frango')
 
 
-    const produtoTexto = "Coxas e Sobrecoxas de Frango com osso e pele Sadia Caixa 15Kg -Caixa c/1";
-    const produto = page.locator(`text="${produtoTexto}"`);
-    await produto.click();
+    const produtoTexto = "Coxas e Sobrecoxas de Frango com osso e pele Sadia Caixa 15Kg -Caixa c/1"
+    const produto = page.locator(`text="${produtoTexto}"`)
+    await produto.click()
 
-    const btnAdicionarCarrinho = page.locator('button:has-text("Adicionar ao carrinho")');
-    await btnAdicionarCarrinho.click();
+    const btnAdicionarCarrinho = page.locator('button:has-text("Adicionar ao carrinho")')
+    await btnAdicionarCarrinho.click()
 
-    const toast = page.locator('.toast-container');
+    const toast = page.locator('.toast-container')
     await expect(toast).toContainText('Adicionado ao carrinho', { timeout: 10000 })
 })
+
+test('deve exibir o produto no carrinho após adicioná-lo', async ({ page }) => {
+
+    await page.goto('https://qas.central-brf.com.br/login')
+
+    await page.getByPlaceholder("e-mail").fill('terezinha.075@uorak.com')
+    await page.getByPlaceholder("Senha").fill('Central@24')
+    await page.locator('button:has-text("Entrar")').click()
+
+    const loginUser = page.locator('.menu-trigger-p.slds-text-align_right.slds-truncate.slds-text-title_bold.lwc-5uc1gleli5')
+    await expect(loginUser).toHaveText("TEREZINHA DE JESUS MENDES GAMA")
+
+    await page.goto('https://qas.central-brf.com.br/global-search/Coxas%20e%20Sobrecoxas%20de%20Frango')
+
+    const produtoTexto = "Coxas e Sobrecoxas de Frango com osso e pele Sadia Caixa 15Kg -Caixa c/1"
+    await page.locator(`text="${produtoTexto}"`).click()
+
+    const btnAdicionarCarrinho = page.locator('button:has-text("Adicionar ao carrinho")')
+    await btnAdicionarCarrinho.click()
+
+    const toast = page.locator('.toast-container')
+    await expect(toast).toContainText('Adicionado ao carrinho', { timeout: 10000 })
+
+    const verCarrinhoBtn = page.locator('button[aria-label="Ver Carrinho"]')
+    await verCarrinhoBtn.click()
+
+    await expect(page).toHaveURL(/.*\/cart/)
+
+    const itemCarrinho = page.locator(`text="${produtoTexto}"`)
+    await expect(itemCarrinho).toBeVisible()
+});
+
+
 
 
 
